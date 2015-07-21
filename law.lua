fda185f4a9ef082de95304bab7de7831a4e6e337
xmorg <jobs@cooperlabs.net>
2015-07-21 14:32:26 -0700

    Update law.lua

--- law.lua
+++ law.lua
@@ -1,6 +1,16 @@
 laws = {
   --bans
   speach = "off",
-  fishing = "off",
-  hunting = "off"
+  fishing = "off", -- off/tax/banned
+  hunting = "off",
+  gardening = "off",
+  fires = "off",
+  wandering = "off", --force villagers to stay in once place unless working, eating, going home.
+  curfew = "off", --force villagers to gather at fire and stay there through the night, stops work.
+  
+  
+  penalties_theft = "none", --fines/whips/hanging/burning/beheading
+  penalties_violate_ban = "none", --fines/whips/hanging/burning/beheading
+  penalties_murder = "none", --fines/whips/hanging/burning/beheading
+  penalties_arson = "none", --fines/whips/hanging/burning/beheading
 }
