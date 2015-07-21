8c238d04a6f8c987ccd2cc1e759d80dbfdcc8c49
xmorg <jobs@cooperlabs.net>
2015-07-21 14:40:33 -0700

    Update law.lua

--- law.lua
+++ law.lua
@@ -5,6 +5,8 @@ laws = {
   hunting = "off",
   gardening = "off",
   fires = "off",
+  holidays = "on" --allow for holidays, decresase production, and a lot of other strange things.
+  parties = "on" --allow for parties, decreases production
   wandering = "off", --force villagers to stay in once place unless working, eating, going home.
   curfew = "off", --force villagers to gather at fire and stay there through the night, stops work.
   
