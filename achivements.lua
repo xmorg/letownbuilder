a64802b04efa8dae89cbbe250e96677deecc21eb
xmorg <jobs@cooperlabs.net>
2015-08-29 20:30:22 -0700

    Update achivements.lua

--- achivements.lua
+++ achivements.lua
@@ -1,3 +1,11 @@
 --stuff
 achivements = {
+  {"Food horder.", "Collect 100 food.", score = 0 },
+  {"Nice buffet", "Have more than 5 types of food", score=0},
+  {"Worlds biggest BBQ.", "Barn catches on fire.", score = 0 },
+  {"Everybody's dead", "Let everyone die.", score = 0 },
+  {"G-g-g-ghost!", "Spot a departed loved one at night.", score = 0 },
+  {"Who took my food!?", "Catch a bandit stealing your food.", score = 0 }, --villagers.lua,350+
+  {"The howling.", "Survive a night with nightwolves", score = 0 },
+  {"Nightwolf slayer", "Hunter kills a nightwolf", score = 0 }
   }
