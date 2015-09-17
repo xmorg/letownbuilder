f5c3c5517db516e950b06645204615805fc2acf3
xmorg <jobs@cooperlabs.net>
2015-09-17 15:12:38 -0700

    Update achivements.lua

--- achivements.lua
+++ achivements.lua
@@ -1,5 +1,6 @@
 --stuff
 achivements = {
+  {"Tree Puncher.", "Cut down your first tree.", score=0}
   {"Food horder.", "Collect 100 food.", score = 0 },
   {"Nice buffet", "Have more than 5 types of food", score=0},
   {"Worlds biggest BBQ.", "Barn catches on fire.", score = 0 },
@@ -8,5 +9,7 @@ achivements = {
   {"Who took my food!?", "Catch a bandit stealing your food.", score = 0 }, --villagers.lua,350+
   {"The howling.", "Survive a night with nightwolves", score = 0 },
   {"Nightwolf slayer", "Hunter kills a nightwolf", score = 0 },
-  {",owNow you make me mad.","Decend into rioting.", score = 0 }
+  {"Now you make me mad.","Decend into rioting.", score = 0 },
+  {"Thriller night", "Everyone becomes a zombie", score = 0 },
+  {"Epic Hunter", "Hunter kills a nightwolf", score = 0}
   }
