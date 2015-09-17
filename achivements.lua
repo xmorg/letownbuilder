6d1b533f0403823cbddc30117617ba46701ba058
xmorg <jobs@cooperlabs.net>
2015-09-17 16:18:14 -0700

    Update achivements.lua

--- achivements.lua
+++ achivements.lua
@@ -14,11 +14,22 @@ base_achivements = {
   {"Epic Hunter", "Hunter kills a nightwolf", score = 0}
   }
 
-achivements = {}
+--achivements = {}
 
 function load_new_achivements()
-  for i,v in ipairs(base_achivements) do
-    table.insert(achivements, base_achivements[i])
-  end
+  achivements = {
+  {"Tree Puncher.", "Cut down your first tree.", score=0},
+  {"Food horder.", "Collect 100 food.", score = 0 },
+  {"Nice buffet", "Have more than 5 types of food", score=0},
+  {"Worlds biggest BBQ.", "Barn catches on fire.", score = 0 },
+  {"Everybody's dead", "Let everyone die.", score = 0 },
+  {"G-g-g-ghost!", "Spot a departed loved one at night.", score = 0 },
+  {"Who took my food!?", "Catch a bandit stealing your food.", score = 0 }, --villagers.lua,350+
+  {"The howling.", "Survive a night with nightwolves", score = 0 },
+  {"Nightwolf slayer", "Hunter kills a nightwolf", score = 0 },
+  {"Now you make me mad.","Decend into rioting.", score = 0 },
+  {"Thriller night", "Everyone becomes a zombie", score = 0 },
+  {"Epic Hunter", "Hunter kills a nightwolf", score = 0}
+  }
 end
 
