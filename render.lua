15f029f2fa375ee8896abfd88f3f75dccaf6aad7
xmorg <jobs@cooperlabs.net>
2015-09-23 15:16:05 -0700

    Update render.lua

--- render.lua
+++ render.lua
@@ -632,8 +632,8 @@ function draw_villagers()
 
 	if game_villagers[i].selected == 1 then
 		love.graphics.setColor(0,0,255,100)
-		love.graphics.circle("fill", game_villagers[i].x-5+game.draw_x, 
-			game_villagers[i].y+5+game.draw_y, 15, 3); 
+		love.graphics.circle("fill", game_villagers[i].x+5+game.draw_x, 
+			game_villagers[i].y+11+game.draw_y, 12, 3); 
 		love.graphics.setColor(255,255,255,255)
 	end
       if game_villagers[i].alive == 0 and is_night() == 1 and kingdom_inventory.graveyards < 1 then
