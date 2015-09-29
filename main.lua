0c4ac81e26be59f42794a01b10430685c17bac1e
xmorg <jobs@cooperlabs.net>
2015-09-29 08:41:19 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -19,6 +19,8 @@ blank_icon = 1
 game = {state = 1, give_direction = "None", day_time = 6000, day_count = 1,
 	tile_selected_x = 0,tile_selected_y = 0, mouse_last_x=0, mouse_last_y=0,
 	fullscreen_mode = "No",
+	tilewidth_fx = 64,  --ground tile width/height, not actual height of tile in pixels
+	tileheight_fx = 32, --but the w/h from tip to tip
 	draw_x=-100, draw_y=-100, screen_width = love.graphics.getWidth(), love.graphics.getHeight(),
 	scroll_speed=3, loc_selected_x = 0, loc_selected_y = 0, --lx+ 32 --ly+ 16
 	tilecount = 32, actor_speed = 2, printx = 0, printy = 0, -- 0  -62-- 536-600
