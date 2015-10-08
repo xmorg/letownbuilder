76c4d69c5b178ce3dd6e9337c5b8bae8680483bb
xmorg <jobs@cooperlabs.net>
2015-10-08 10:14:26 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -658,7 +658,7 @@ function love.draw()
       draw_biome_select() -- select which biome.
    elseif game.show_menu == 7 then --achivements
       game_achivements_draw()
-   elseif game.show_menu = "trading" then
+   elseif game.show_menu == "trading" then
    	show_transaction_menu()
    else	
       game.screen_height = love.graphics.getHeight()
