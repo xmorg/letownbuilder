96b9b0f38e2131bf474961709b9d5eff3c8db996
xmorg <jobs@cooperlabs.net>
2015-10-09 14:45:51 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -601,10 +601,10 @@ function love.keypressed(key)
 		if key == "left" then merchant_window.active_inventory = "kingdom" end
 		if key == "right" then merchant_window.active_inventory = "merchant" end
 		if key == "up" then --fixme come all the way up.
-			if merchant_window.active_inventory == "merchant" and merchant_window.selected_merchant_item > 0 then 
+			if merchant_window.active_inventory == "merchant" and merchant_window.selected_merchant_item > 1 then 
 				merchant_window.selected_merchant_item=merchant_window.selected_merchant_item-1
 			end
-			if merchant_window.active_inventory == "kingdom" and merchant_window.selected_town_item > 0 then
+			if merchant_window.active_inventory == "kingdom" and merchant_window.selected_town_item > 1 then
 				merchant_window.selected_town_item=merchant_window.selected_town_item-1
 			end
 		end
