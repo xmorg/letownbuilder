522bf3b7ae9e12d9dbb25eafa64cf96c9a684e49
xmorg <jobs@cooperlabs.net>
2015-10-09 10:41:49 -0700

    Update merchants.lua

--- merchants.lua
+++ merchants.lua
@@ -80,7 +80,7 @@ function show_transaction_menu() --allows you to choose what you are selling.
 	love.graphics.setColor(0,0,0,255)
 	for i,v in ipairs(lookup_table) do
 		if i > merchant_window.scroll_offset and i < merchant_window.scroll_offset+merchant_window.scroll_offset_max_items then
-			if i == merchant_window.selected_town_item then 
+			if i == merchant_window.selected_town_item and merchant_window.active_inventory == "kingdom" then 
 				love.graphics.setColor(100,100,255,255)
 				love.graphics.rectangle("fill", tx+10, ty+10, townframe_x, 15) --town window
 				love.graphics.setColor(0,0,0,255)
