cf73f61749e34a9499ef6ee9910e7bce8d704b96
xmorg <jobs@cooperlabs.net>
2015-10-19 03:56:47 -0700

    Update merchants.lua

--- merchants.lua
+++ merchants.lua
@@ -81,6 +81,7 @@ function show_transaction_menu() --allows you to choose what you are selling.
 	end
 	love.graphics.rectangle("fill", tx+10+townframe_x+10, ty+10, townframe_x, townframe_y) --merchants window
 	love.graphics.setColor(0,0,0,255) --now list the town items at tx+10
+	love.graphics.print("Your town",tx+20,ty-10+(text_width))
 	for i,v in ipairs(lookup_table) do
 		if i >= merchant_window.scroll_offset and i <= merchant_window.scroll_offset+merchant_window.scroll_offset_max_items then
 			if i == merchant_window.selected_town_item and merchant_window.active_inventory == "kingdom" then 
@@ -91,7 +92,8 @@ function show_transaction_menu() --allows you to choose what you are selling.
 			love.graphics.print(lookup_table[i].." ("..kingdom_inventory_forsale[i]..")",
 			tx+20,  ty-10+(text_width*i)+text_width) 
 		end
-	end 
+	end
+	love.graphics.print("Merchants",tx+20+townframe_x+10, ty-10+(text_width)) --merchants window
 	for i,v in ipairs(lookup_table) do --merchant inventory
 		if i >= merchant_window.scroll_offset and i <= merchant_window.merchant_scroll_offset+merchant_window.scroll_offset_max_items then
 		if i == merchant_window.selected_merchant_item and merchant_window.active_inventory == "merchant" then 
